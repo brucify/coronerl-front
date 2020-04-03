58245aefcae389b6b4e4af300c0b7bc3bd342076
Bruce Yinhe <bruce.yinhe@gmail.com>
2020-04-03 22:58:53 +0200

    CoronaDrawer.js++

--- src/components/CoronaDrawer.js
+++ src/components/CoronaDrawer.js
@@ -1,11 +1,70 @@
 import React from "react"
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import PublicIcon from '@material-ui/icons/Public';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
 
 export default () => {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Global', 'Sweden', 'USA'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon><PublicIcon /></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  var anchor = "left";
   return (
-    <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton>
+    <React.Fragment key={anchor}>
+      <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu" onClick={toggleDrawer(anchor, true)}>
+        <MenuIcon />
+      </IconButton>
+      <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+        {list(anchor)}
+      </Drawer>
+    </React.Fragment>
   )
 }
